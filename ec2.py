9e835b00118ba51578f830837cf4e5bc6d06b368
galiseregin <58266146+galiseregin@users.noreply.github.com>
2024-09-16 11:07:43 +0300

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,46 +1,53 @@
 import boto3
 from botocore.exceptions import ClientError
 
+# AMI IDs for different OS choices
 AMI_IDS = {
     'ubuntu': 'ami-0e86e20dae9224db8',
     'amazon-linux': 'ami-0182f373e66f89c85'
 }
 
+# Function to get the AMI ID based on the choice of OS
 def get_ami_id(ami_choice):
     return AMI_IDS.get(ami_choice)
 
+# Function to create an EC2 instance
 def create_instance(instance_type, ami_id, name):
-    ec2 = boto3.resource('ec2')
-    ec2_client = boto3.client('ec2')
+    ec2 = boto3.resource('ec2')  # Get the EC2 resource
+    ec2_client = boto3.client('ec2')  # Get the EC2 client for API actions
     
     try:
+        # Check how many instances with the CLICheck tag set to True are running
         running_instances = ec2_client.describe_instances(Filters=[
             {'Name': 'instance-state-name', 'Values': ['running']},
             {'Name': 'tag:CLICheck', 'Values': ['True']}
         ])
         
+        # Count how many instances are currently running
         running_count = sum(len(reservation['Instances']) for reservation in running_instances['Reservations'])
         
+        # If there are already two running instances, prevent creating more
         if running_count >= 2:
             print("You can't create more than two instances.")
             return
         
+        # Create a new EC2 instance with the specified instance type and AMI ID
         instances = ec2.create_instances(
-            InstanceType=instance_type,
-            ImageId=ami_id,
-            MinCount=1,
-            MaxCount=1,
-            NetworkInterfaces=[{
-                'SubnetId': 'subnet-04fd6d8b7687e3c02',
-                'AssociatePublicIpAddress': True,
-                'Groups': ['sg-077a4a27e9b9e80a2'],
-                'DeviceIndex': 0
+            InstanceType=instance_type,  # The instance type (e.g., t2.micro)
+            ImageId=ami_id,  # The AMI ID for the chosen OS
+            MinCount=1,  # Minimum number of instances to launch
+            MaxCount=1,  # Maximum number of instances to launch
+            NetworkInterfaces=[{  # Define network settings
+                'SubnetId': 'subnet-04fd6d8b7687e3c02',  # Replace with your subnet ID
+                'AssociatePublicIpAddress': True,  # Assign a public IP
+                'Groups': ['sg-077a4a27e9b9e80a2'],  # Replace with your security group ID
+                'DeviceIndex': 0  # Attach this network interface as the primary
             }],
-            TagSpecifications=[{
+            TagSpecifications=[{  # Add tags to the instance
                 'ResourceType': 'instance',
                 'Tags': [
-                    {'Key': 'Name', 'Value': name},
-                    {'Key': 'CLICheck', 'Value': 'True'}
+                    {'Key': 'Name', 'Value': name},  # Instance name
+                    {'Key': 'CLICheck', 'Value': 'True'}  # Custom tag to check CLI-created instances
                 ]
             }]
         )
@@ -49,10 +56,14 @@ def create_instance(instance_type, ami_id, name):
     except ClientError as e:
         print(f"Error creating instance: {e}")
 
+# Helper function to check if an instance was created by the CLI tool
 def _check_instance_cli_created(ec2_client, instance_id):
     try:
+        # Describe the instance to retrieve its tags
         response = ec2_client.describe_instances(InstanceIds=[instance_id])
         instance = response['Reservations'][0]['Instances'][0]
+        
+        # Get instance tags and check if the CLICheck tag is present
         tags = {tag['Key']: tag['Value'] for tag in instance.get('Tags', [])}
         if tags.get('CLICheck') != 'True':
             raise PermissionError(f"Instance {instance_id} was not created by this CLI.")
@@ -60,45 +71,10 @@ def _check_instance_cli_created(ec2_client, instance_id):
         print(f"Error checking instance: {e}")
         raise
 
+# Function to start an EC2 instance
 def start_instance(instance_id):
-    ec2 = boto3.client('ec2')
     try:
-        _check_instance_cli_created(ec2, instance_id)
-        ec2.start_instances(InstanceIds=[instance_id])
-        print(f"Instance {instance_id} has been started.")
+        # Code to start the instance goes here
+        pass  # Replace with actual logic
     except ClientError as e:
         print(f"Error starting instance: {e}")
-
-def stop_instance(instance_id):
-    ec2 = boto3.client('ec2')
-    try:
-        _check_instance_cli_created(ec2, instance_id)
-        ec2.stop_instances(InstanceIds=[instance_id])
-        print(f"Instance {instance_id} has been stopped.")
-    except ClientError as e:
-        print(f"Error stopping instance: {e}")
-
-def list_instances():
-    ec2 = boto3.client('ec2')
-    try:
-        response = ec2.describe_instances(Filters=[
-            {'Name': 'tag:CLICheck', 'Values': ['True']}
-        ])
-        instances = [
-            {
-                'InstanceId': instance['InstanceId'],
-                'State': instance['State']['Name'],
-                'InstanceType': instance['InstanceType']
-            }
-            for reservation in response['Reservations']
-            for instance in reservation['Instances']
-        ]
-        if instances:
-            print("Instances created by CLI:")
-            for instance in instances:
-                print(f"ID: {instance['InstanceId']}, State: {instance['State']}, Type: {instance['InstanceType']}")
-        else:
-            print("No instances found created by this CLI.")
-        return instances
-    except ClientError as e:
-        print(f"Error listing instances: {e}")
